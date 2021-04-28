085463204e4c43b0393f34ac46392515c6c6a1f4
mamuratova <mamuratovad@gmail.com>
2021-04-28 21:11:22 +0600

    ff

--- bot.py
+++ bot.py
@@ -11,13 +11,10 @@ btn2 = InlineKeyboardButton('Нет', callback_data='no')
 inline_keyboard.add(btn1, btn2)
 
 
-
-
 @bot.message_handler(commands=['start'])
 def starting(message):
     chat_id = message.chat.id
     user = message.from_user.first_name
-    bot.delete_message(chat_id, message.message_id)
     bot.send_message(chat_id, f'Hi, {user}')
     bot.send_sticker(chat_id, 'CAACAgIAAxkBAAEBOklgiOlR-KFj2lEU9nMlPSGJ2zKIOQACpAADr8ZRGgTuYv70faXbHwQ')
     bot.send_message(chat_id, "Хочешь поиграть:", reply_markup=inline_keyboard)
@@ -41,30 +38,29 @@ def func(c):
 def game(message):
     chat_id = message.chat.id
     text = message.text
-    global attempt
     try:
-        if 5 != attempt and attempt < 5:
+        global attempt
+        if 6 != attempt and attempt < 6:
 
             if int(text) == rnum:
                 attempt += 1
-                bot.send_message(chat_id, f'{rnum}')
                 bot.send_message(message.chat.id, 'Поздравляю! Ты угадал!')
                 bot.send_sticker(message.chat.id, 'CAACAgIAAxkBAAEBOolgiULKH1_eCj2f4RVZBbIh0T3OQAACIgMAAm2wQgO8x8PfoXC1eB8E')
                 bot.send_message(chat_id, f'Ты использовал {attempt} попыток!')
                 bot.send_message(chat_id, 'Хочешь сыграть еще раз?', reply_markup=inline_keyboard)
-            if int(text) != rnum:
+            elif int(text) != rnum:
                 attempt += 1
                 if int(text) > rnum:
                     bot.send_message(chat_id, 'Загаданное число меньше чем ты думаешь!')
                     bot.send_sticker(chat_id, 'CAACAgIAAxkBAAEBOoxgiUi0VGil2euTtCfFyAj53WHvyAACEwMAAm2wQgMrGNM75XhwfB8E')
                     bot.send_message(chat_id, f'Ты использовал {attempt} попыток!')
-                if int(text) < rnum:
+                elif int(text) < rnum:
                     bot.send_message(chat_id, 'Загаданное число больше чем ты думаешь!')
                     bot.send_sticker(chat_id, 'CAACAgIAAxkBAAEBOqRgiUkaG3a0vlf-7pFqcoci90pdjAACDAMAAm2wQgNUfsxB8ZeE-x8E')
                     bot.send_message(chat_id, f'Ты использовал {attempt} попыток!')
         else:
             bot.send_message(chat_id, 'Ты проиграл!!!')
-            bot.send_message(chat_id, f'Ты использовал {attempt+1} попыток!')
+            bot.send_message(chat_id, f'Ты использовал {attempt} попыток!')
             bot.send_sticker(chat_id, 'CAACAgIAAxkBAAEBOrlgiUutzNC-SOvJirp0Xzij_E4zhAACzgwAApmBUUu-SEJ4AYw9hR8E')
             bot.send_message(chat_id, 'Хочешь сыграть еще раз?', reply_markup=inline_keyboard)
     except Exception as e:
